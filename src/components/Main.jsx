da85a41d0075f859dfdf56e63d3655eeb716e1e4
Monicah Cloud <monicahcloud@gmail.com>
2023-08-31 20:25:11 -0500

    working on css media query

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,50 +7,50 @@
 // import pic3 from "../images/20220820_121848.jpg"
 // import pic4 from "../images/20220820_121857.jpg"
 // import pic5 from "../images/20220820_181851.jpg"
-// import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import { Container, Col } from 'react-bootstrap';
+
 
 const Main = () => {
 
-    // const images = [
-    //     {url:"https://scontent-ord5-1.xx.fbcdn.net/v/t39.30808-6/272920895_10159826855581118_4598242148598824615_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=ioD91qwOHjEAX8oER5L&_nc_ht=scontent-ord5-1.xx&oh=00_AfC4gn6g4tVaaDM-Sm3wkdnc7u7umMAr0UHC-zRdExtsqQ&oe=64EBF643"},
-    //     {url:"https://scontent-ord5-2.xx.fbcdn.net/v/t39.30808-6/272874157_10159826855466118_8957006912199979987_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=y-4xaZyt3AkAX8DYlZK&_nc_ht=scontent-ord5-2.xx&oh=00_AfBwlZz5b5YF1zA27P7KSEe4rj33VflT5l9P_RqcdIM0dA&oe=64EC8DF9"},
-    //     {url:"https://scontent-ord5-2.xx.fbcdn.net/v/t39.30808-6/293537889_1016050327341118_3285235071669850506_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=174925&_nc_ohc=CNvZA5gN_1kAX935w7j&_nc_ht=scontent-ord5-2.xx&oh=00_AfA-f6jdVdJEUDShSqfXOvXr02Vu_H9Vwp5b0-yx7n74SA&oe=64EC0F57"},
-    //     {url:"https://scontent-ord5-2.xx.fbcdn.net/v/t39.30808-6/300576159_10159706813221865_7319814243983167033_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=rWl0Z3mz9xkAX_9_IBe&_nc_ht=scontent-ord5-2.xx&oh=00_AfC6whn5awmRcrodCgRhgjjYIac-rZvRsvSfbD_xepUQSA&oe=64ED7F16"}
+    const images = [
+        {url:"https://scontent-ord5-1.xx.fbcdn.net/v/t39.30808-6/272920895_10159826855581118_4598242148598824615_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=ioD91qwOHjEAX8oER5L&_nc_ht=scontent-ord5-1.xx&oh=00_AfC4gn6g4tVaaDM-Sm3wkdnc7u7umMAr0UHC-zRdExtsqQ&oe=64EBF643"},
+        {url:"https://scontent-ord5-2.xx.fbcdn.net/v/t39.30808-6/272874157_10159826855466118_8957006912199979987_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=y-4xaZyt3AkAX8DYlZK&_nc_ht=scontent-ord5-2.xx&oh=00_AfBwlZz5b5YF1zA27P7KSEe4rj33VflT5l9P_RqcdIM0dA&oe=64EC8DF9"},
+        {url:"https://scontent-ord5-1.xx.fbcdn.net/v/t39.30808-6/273014796_10159826855271118_6316802090697066875_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5614bc&_nc_ohc=pEujkej08-AAX9Ocwny&_nc_ht=scontent-ord5-1.xx&oh=00_AfAAfVzcLV_lbMYj-e6DzgscExMtzqz-BNZW-LXI3exeBA&oe=64F6D2D9"},
+        {url:"https://scontent-ord5-2.xx.fbcdn.net/v/t39.30808-6/272871397_10159826855606118_2925405232570321899_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=5614bc&_nc_ohc=5FZfBOUCoTcAX-5n2yk&_nc_ht=scontent-ord5-2.xx&oh=00_AfBfgTy0tDn0GWXyZ3krVpEaYs22Eol-C32H8cnfJm42Rg&oe=64F6F69A"}
+ 
+    ];
+    const delay = 2500;
     
-    
-    // ];
-    // const delay = 2500;
-    
-    // const [index, setIndex] = useState(0);
-    // const timeoutRef = useRef(null);
+    const [index, setIndex] = useState(0);
+    const timeoutRef = useRef(null);
 
-    // function resetTimeout() {
-    //     if (timeoutRef.current) {
-    //         clearTimeout(timeoutRef.current);
-    //     }
-    // }
+    function resetTimeout() {
+        if (timeoutRef.current) {
+            clearTimeout(timeoutRef.current);
+        }
+    }
 
-    // useEffect(() => {
-    //     resetTimeout();
-    //     timeoutRef.current = setTimeout(
-    //         () =>
-    //             setIndex((prevIndex) =>
-    //                 prevIndex === images.length - 1 ? 0 : prevIndex + 1
-    //             ),
-    //         delay
-    //     );
+    useEffect(() => {
+        resetTimeout();
+        timeoutRef.current = setTimeout(
+            () =>
+                setIndex((prevIndex) =>
+                    prevIndex === images.length - 1 ? 0 : prevIndex + 1
+                ),
+            delay
+        );
 
-    //     return () => {
-    //         resetTimeout();
-    //     };
-    // }, [index]);
+        return () => {
+            resetTimeout();
+        };
+    }, [index]);
 
     return (
         <>
-        {/* <div className="slideshow">
-           
+                <br/>
             <div
-                className="slideshowSlider"
+                className=" slideshow slideshowSlider"
                 style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
             >
                 {images.map((pic, index) => (
@@ -58,10 +58,13 @@ const Main = () => {
                         className="slide"
                         key={index}
                     >
-                        <img src={pic.url} alt=""/></div>
+                         <img
+            className="rounded-pill"
+              width="350"
+             src={pic.url} alt=""/></div>
                 ))}
             </div>          
-            </div> */}
+           
              <div  className=" col-md-12">
 
                 <div>
